ba442bf4af95481b57f89f15f0eeac9c6c201b22
Nikolay Rademacher <hrrdx33@gmail.com>
2021-10-05 17:10:34 +0200

    feat: implement context.ts

--- src/context.ts
+++ src/context.ts
@@ -1,17 +1,30 @@
-import { PrismaClient, User } from '@prisma/client'
-import * as jwt from 'jsonwebtoken'
-import { config, Depromisify } from './utils'
+import { Http2ServerRequest } from 'http2';
+import { config, Depromisify } from './utils';
+import * as jwt from 'jsonwebtoken';
 
-export const prisma = new PrismaClient()
+export interface ResolverContext {
+  is_authed: boolean;
+  user_id?: string;
+}
 
-export const context = async ({ req }) => {
-  let token: string = req?.headers?.authorization || ''
+export const context = async ({ req }: { req: Http2ServerRequest }) => {
+  const ctx: ResolverContext = {
+    is_authed: false,
+  };
+  let token: string = req?.headers?.authorization || '';
 
   if (token) {
-    if (token.startsWith('Bearer ')) token = token.split(' ')[1]
+    if (token.startsWith('Bearer ')) token = token.split(' ')[1];
 
-    const decoded = jwt.verify(token, config.TOKEN_SECRET)
+    const decoded = jwt.verify(token, config.TOKEN_SECRET);
+
+    if (typeof decoded === 'string') return ctx;
+
+    if (decoded.user_id) {
+      ctx.is_authed = true;
+      ctx.user_id = decoded.user_id;
+    }
   }
-}
+};
 
 export type Context = Depromisify<typeof context>;
