fd2653060c84971529fd7972c1dee19b5b9a1115
Nikolay Rademacher <hrrdx33@gmail.com>
2021-10-07 21:31:42 +0200

    chore: update dockerfile and sort deps

--- src/modules/user/resolvers.ts
+++ src/modules/user/resolvers.ts
@@ -6,9 +6,9 @@ import { db } from '../../database';
 export const UserResolvers: Resolvers<Context> = {
   Query: {
     me: async (_parent, _args, context) => {
-      if (!context?.user_id) throw new AuthenticationError('missing_token');
+      if (!context?.userId) throw new AuthenticationError('missing_token');
 
-      return db.user.findUnique({ where: { id: context.user_id } });
+      return db.user.findUnique({ where: { id: context.userId } });
     },
   },
 };
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import { ApolloServer } from 'apollo-server';
-import { schema } from './modules';
-import { context } from './context';
-import { formatError } from './utils';
+import { ApolloServer } from 'apollo-server'
+import { schema } from './modules'
+import { context } from './context'
+import { formatError } from './utils'
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000
 
 new ApolloServer({ schema, context, formatError }).listen({ port }, () =>
-  console.log(`Server ready at: http://localhost:${port}`),
-);
+  console.log(`Server ready at: http://localhost:${port}`)
+)
