0797ab8fd580f6de3a8187583d824e8c37236a60
Nikolay Rademacher <hrrdx33@gmail.com>
2021-10-30 17:45:31 +0200

    chore: update file-ops scripts with silent flags

--- src/modules/contexts.ts
+++ src/modules/contexts.ts
@@ -2,10 +2,10 @@ import type { UserResolverContext } from './user';
 import { mergeContexts } from '#internal/utils';
 import { userContext } from './user';
 
-// extend with type union
+// extend with type unions
 export type SingleModuleContext = UserResolverContext;
 
-// extend with type inersection
+// extend with type intersections
 export type AllModulesContext = UserResolverContext;
 
 export const moduleContexts = mergeContexts(userContext);
