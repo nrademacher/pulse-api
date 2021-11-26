cbe209b2fbf0cc6fde7f32e3c2c1bbfedba1c996
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2021-11-26 21:26:16 +0100

    feat: use graphql redis subscriptions

--- src/services/prisma.ts
+++ src/services/prisma.ts
@@ -1,7 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-import { PubSub } from 'graphql-subscriptions'
 
 export const prisma = new PrismaClient()
 export const prismaTestClient = new PrismaClient()
 
-export const pubsub = new PubSub()
---
+++ src/services/pubsub.ts
@@ -0,0 +1,17 @@
+import { RedisPubSub } from 'graphql-redis-subscriptions';
+import Redis from 'ioredis';
+
+const options = {
+  host: process.env.DB_HOST || "localhost",
+  port: 6379,
+  retryStrategy: (times: number) => {
+    // reconnect after
+    return Math.min(times * 50, 2000);
+  }
+};
+
+export const pubsub = new RedisPubSub({
+  publisher: new Redis(options),
+  subscriber: new Redis(options)
+});
+
