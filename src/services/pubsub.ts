be0779023abcbfbb2ceae84da8adfb640012b511
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2021-11-27 17:46:26 +0100

    chore: use non-redis pubsub for tests

--- src/services/pubsub.ts
+++ src/services/pubsub.ts
@@ -1,15 +1,22 @@
+import { PubSub } from 'graphql-subscriptions'
 import { RedisPubSub } from 'graphql-redis-subscriptions'
 import Redis from 'ioredis'
 
-const options = {
-  host: process.env.DB_HOST || 'localhost',
-  port: 6379,
-  /* retryStrategy: (times: number) => {
-    return Math.min(times * 50, 2000)
-  }, */
-}
+export let pubsub: PubSub | RedisPubSub
 
-export const pubsub = new RedisPubSub({
-  publisher: new Redis(options),
-  subscriber: new Redis(options),
-})
+if (process.env.DB_ENV === 'test') {
+  pubsub = new PubSub()
+} else {
+  const options = {
+    host: process.env.DB_HOST || 'localhost',
+    port: 6379,
+    retryStrategy: (times: number) => {
+      return Math.min(times * 50, 2000)
+    },
+  }
+
+  pubsub = new RedisPubSub({
+    publisher: new Redis(options),
+    subscriber: new Redis(options),
+  })
+}
