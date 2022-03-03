19a74aaaf50ab47e00e3b61357a1a32bc20b07d6
Nikolay Rademacher <rademacher.nikolay@gmail.com>
2022-03-04 00:16:43 +0100

    add lint-staged, update prettier config

---
+++ prettier.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  tabWidth: 4,
+  printWidth: 120,
+  proseWrap: 'preserve',
+  semi: false,
+  trailingComma: 'es5',
+  singleQuote: true,
+  arrowParens: 'avoid',
+  tailwindConfig: './tailwind.config.js',
+  overrides: [
+    {
+      files: '{*.js?(on),*.js,*.y?(a)ml,.*.js?(on),.*.y?(a)ml,*.md,.prettierrc,.stylelintrc,.babelrc}',
+      options: {
+        tabWidth: 2,
+      },
+    },
+    {
+      files: '{**/tsconfig.json,**/tsconfig.*.json}',
+      options: {
+        parser: 'json5',
+        quoteProps: 'preserve',
+        singleQuote: false,
+        trailingComma: 'all',
+      },
+    },
+  ],
+}
+
